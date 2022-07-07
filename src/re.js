15e3d06f8473fd8485306513c116c38a76850a34
Efrel Armando López Cáceres <programmer@ferrebaratillohn.com>
2022-07-07 09:46:41 -0600

    Reasignación y redeclaración

---
+++ src/re.js
@@ -0,0 +1,32 @@
+var firstName; //undefined
+firstName = "Efrel";
+console.log(firstName);
+
+var lastName = "Pedro"; // declaration / assignment
+lastName = "Pablo"; // reassignment
+console.log(lastName);
+
+var secondName = "David"; // declaration / assignment
+var secondName = "Ana"; // reassignment
+console.log(secondName);
+
+//let
+let fruit = "Apple"; // declaration / assignment
+fruit = "Kiwi"; // reassignment
+console.log(fruit);
+
+let fruit = "Orange"; // declaration / assignment
+console.log(fruit);
+
+// const
+const animal = "Dog"; // declaration / assignment
+//animal = "Cat"; // reassignment
+const animal = "Snake"; // reassignment
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
