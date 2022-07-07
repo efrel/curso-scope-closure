560f53f9e6a54ef2600d80becbd4b3cf466450ec
Efrel Armando López Cáceres <programmer@ferrebaratillohn.com>
2022-07-07 09:25:05 -0600

    block scope

---
+++ src/block.js
@@ -0,0 +1,13 @@
+function fruits() {
+    if (true) {
+        var fruit1 = "apple";
+        let fruit2 = "orange";
+        const fruit3 = "banana";
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+
+    console.log(fruit1);
+}
+
+fruits();
