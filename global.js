246a0eb38b1965bebe7c914ee511232ebb51a4ad
Efrel Armando López Cáceres <programmer@ferrebaratillohn.com>
2022-07-07 09:10:35 -0600

    archivo global scope

---
+++ global.js
@@ -0,0 +1,15 @@
+// variables
+
+var a; // declaration variables
+var b = "b"; // declaration variables | assignment variables
+b = "bb"; //reassignment variables
+var a = "a"; // redeclaration variables
+
+// global scope variables
+var fruit = "fruit"; //global
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
